125ad7d6c08e18555623f8256425204e47ed075a
AndyHou <andyhou965@gmail.com>
2024-08-16 22:00:13 +0400

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Startup Landing Page",
+  description:
+    "A landing page for an AI startup created with cutting-edge technologies.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(inter.className, "bg-black text-white antialiased")}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
